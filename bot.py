4c01220ab78f5b6a0d1682fb3d2001ed49e1bd85
sukhwinder04 <singhsukhwinder79157@gmail.com>
2025-01-24 19:28:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,11 +59,22 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/ultroid_official")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \n @iTz_Ayanokoji")
         self.LOGGER(__name__).info(f""" \n\n       
-(っ◔◡◔)っ ♥ ULTROIDOFFICIAL ♥
-░╚════╝░░╚════╝░╚═════╝░╚══════╝
-                                          """)
+        
+        
+███████╗ █████╗ ███╗   ██╗██████╗  ██╗  ██╗██╗   ██╗
+██╔════╝██╔══██╗████╗  ██║██╔══██╗██║  ██║██║   ██║
+███████╗███████║██╔██╗ ██║██║  ██║███████║██║   ██║
+╚════██║██╔══██║██║╚██╗██║██║  ██║██╔══██║██║   ██║
+███████║██║  ██║██║ ╚████║██████╔╝██║  ██║╚██████╔╝
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝ 
+                                                   
+        
+        
+        
+        
+        """)
         self.username = usr_bot_me.username
         #web-response
         app = web.AppRunner(await web_server())
