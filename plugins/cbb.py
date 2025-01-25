ed92b6dac83a07ccc0a8a5a433d6825c759bfd8a
sukhwinder04 <singhsukhwinder79157@gmail.com>
2025-01-25 18:27:33 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>╭━━━━━━━━━━━━━━━➣\n┣⪼ Owner : <a href='tg://user?id=5745818770'>Vergil</a>\n┣⪼ Bot Updates : <a href'https://t.me/ikigai_bots'>IKigai</a>\n┣⪼ Support Channel: <a href='https://t.me/ikigai_chats'>Ikigai Support</a>\n┣⪼ Our Network :<a href='https://t.me/ikigai_Network'>Ikigai Network</a>\n┣⪼ Movies Channel :<a href='https://t.me/ikigai_Movies'>Ikigai Movies</a>\n╰───────────────⍟</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
