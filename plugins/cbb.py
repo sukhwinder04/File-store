11d4596a782b65967dcb0597ba3c5eb09cfa9d9d
sukhwinder04 <singhsukhwinder79157@gmail.com>
2025-01-25 21:59:38 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>╭━━━━━━━━━━━━━━━➣\n┣⪼ 𝙾𝚠𝚗𝚎𝚛 : <a href='tg://user?id={OWNER_ID}'>𝓢𝓐𝓝𝓓𝓗𝓤</a>\n┣⪼ 𝙾𝚝𝚝 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 : <a href='https://t.me/OttSandhu'>𝓞𝓽𝓽 𝓢𝓪𝓷𝓭𝓱𝓾</a>\n┣⪼ 𝙾𝚝𝚝 𝙶𝚛𝚘𝚞𝚙 :<a href='https://t.me/+_-9trQQYYFczNTJl'>𝓞𝓽𝓽 𝓖𝓻𝓸𝓾𝓹</a>\n┣⪼ 𝙼𝚘𝚟𝚒𝚎𝚜 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 :<a href='https://t.me/+MbS71p0fCIRhMTA1'>𝓟𝓾𝓷𝓳𝓪𝓫𝓲 𝓜𝓸𝓿𝓲𝓮</a>\n╰───────────────⍟</b>",
+            text = f"<b>╭━━━━━━━━━━━━━━━➣\n┣⪼ 𝙾𝚠𝚗𝚎𝚛 : <a href='tg://user?id={OWNER_ID}'>𝑽𝒊𝒔𝒉𝒂𝒍</a>\n┣⪼ 𝙾𝚝𝚝 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 : <a href='https://t.me/OttSandhu'>𝙊𝙩𝙩 𝙎𝙖𝙣𝙙𝙝𝙪</a>\n┣⪼ 𝙾𝚝𝚝 𝙶𝚛𝚘𝚞𝚙 : <a href='https://t.me/+_-9trQQYYFczNTJl'>𝙊𝙩𝙩 𝙂𝙧𝙤𝙪𝙥</a>\n┣⪼ 𝙼𝚘𝚟𝚒𝚎𝚜 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 : <a href='https://t.me/+MbS71p0fCIRhMTA1'>𝙋𝙪𝙣𝙟𝙖𝙗𝙞 𝙈𝙤𝙫𝙞𝙚</a>\n╰───────────────⍟</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
