fd726b3bf147c1659582ab61c8a16d2fce7154a4
sukhwinder04 <singhsukhwinder79157@gmail.com>
2025-02-10 20:23:34 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -225,7 +225,7 @@ REPLY_ERROR = """<code>Use this command as a replay to any telegram message with
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
     invite = await client.create_chat_invite_link(
-        chat_id=FORCE_SUB_CHANNEL1,
+        chat_id=FORCE_SUB_CHANNEL,
         creates_join_request=True
     )
     buttons = [
