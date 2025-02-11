34f494d091b1f74abaa9ad1463afa678b2125794
sukhwinder04 <singhsukhwinder79157@gmail.com>
2025-02-11 20:14:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -51,6 +51,7 @@ TUT_VID = os.environ.get("TUT_VID", "https://t.me/OttSandhu") # shareus ka tut_v
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
+JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", True)
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
