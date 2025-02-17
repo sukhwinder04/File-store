bbc78d70cad368481cb431bb18d7f3dec4ce88aa
sukhwinder04 <singhsukhwinder79157@gmail.com>
2025-02-17 19:45:45 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -198,6 +198,6 @@ async def chk_premium(filter, client, message):
         return False
 
 
-
+req = filters.create(requested)
 premium = filters.create(chk_premium)
 subscribed = filters.create(is_subscribed)
