ae6d41a4a21c6679e3ad7c29ec70e5b111d3d82c
sukhwinder04 <singhsukhwinder79157@gmail.com>
2025-02-18 20:05:30 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -16,7 +16,7 @@ from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from datetime import datetime, timedelta
 from bot import Bot
 from config import *
-from helper_func import subscribed, encode, decode, get_messages, get_shortlink, get_verify_status, update_verify_status, get_exp_time
+from helper_func import subscribed, encode, decode, get_messages, get_shortlink, get_verify_status, update_verify_status, get_exp_time, premium
 from database.database import add_user, del_user, full_userbase, present_user
 from shortzy import Shortzy
 
