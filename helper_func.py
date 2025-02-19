2d81e90f7c001a85e6a8ba2f07554c490191ed55
sukhwinder04 <singhsukhwinder79157@gmail.com>
2025-02-19 19:58:01 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -12,7 +12,7 @@ from shortzy import Shortzy
 import requests
 import time
 from datetime import datetime
-from database.database import user_data, db_verify_status, db_update_verify_status, present_req, present_req2, is_present
+from database.database import user_data, db_verify_status, db_update_verify_status, present_req, present_req2, is_premium
 
 #logger = logging.getLogger(__name__)
 #logger.setLevel(logging.INFO)
