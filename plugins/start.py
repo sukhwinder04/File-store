6829fc232affd0be00263cde8623d2e7d29769af
sukhwinder04 <singhsukhwinder79157@gmail.com>
2025-02-20 20:46:42 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -16,7 +16,7 @@ from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from datetime import datetime, timedelta
 from bot import Bot
 from config import *
-from helper_func import subscribed, encode, decode, get_messages, get_shortlink, get_verify_status, update_verify_status, get_exp_time
+from helper_func import subscribed, encode, decode, get_messages, get_shortlink, get_verify_status, update_verify_status, get_exp_time, premium
 from database.database import add_user, del_user, full_userbase, present_user
 from shortzy import Shortzy
 
@@ -306,6 +306,99 @@ Unsuccessful: <code>{unsuccessful}</code></b>"""
         
         return await pls_wait.edit(status)
 
+
+@Bot.on_message(filters.command('start') & filters.private & subscribed & premium)
+async def start_command(client: Client, message: Message):
+        if not await present_user(id):
+            try:
+                await add_user(id)
+            except:
+                pass
+                
+        if len(message.text) > 7:
+            try:
+                base64_string = message.text.split(" ", 1)[1]
+            except:
+                return
+            _string = await decode(base64_string)
+            argument = _string.split("-")
+            if len(argument) == 3:
+                try:
+                    start = int(int(argument[1]) / abs(client.db_channel.id))
+                    end = int(int(argument[2]) / abs(client.db_channel.id))
+                except:
+                    return
+                if start <= end:
+                    ids = range(start, end+1)
+                else:
+                    ids = []
+                    i = start
+                    while True:
+                        ids.append(i)
+                        i -= 1
+                        if i < end:
+                            break
+            elif len(argument) == 2:
+                try:
+                    ids = [int(int(argument[1]) / abs(client.db_channel.id))]
+                except:
+                    return
+            temp_msg = await message.reply("Please wait...")
+            try:
+                messages = await get_messages(client, ids)
+            except:
+                await message.reply_text("Something went wrong..!")
+                return
+            await temp_msg.delete()
+            
+            phdlusts = []
+            messages = await get_messages(client, ids)
+            for msg in messages:
+                if bool(CUSTOM_CAPTION) & bool(msg.document):
+                    caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
+                else:
+                    caption = "" if not msg.caption else msg.caption.html
+
+                if DISABLE_CHANNEL_BUTTON:
+                    reply_markup = msg.reply_markup
+                else:
+                    reply_markup = None
+                
+                try:
+                    messages = await get_messages(client, ids)
+                    phdlust = await msg.copy(chat_id=message.from_user.id, caption=caption, reply_markup=reply_markup , protect_content=PROTECT_CONTENT)
+                    phdlusts.append(phdlust)
+                    if AUTO_DELETE == True:
+                        #await message.reply_text(f"The message will be automatically deleted in {delete_after} seconds.")
+                        asyncio.create_task(schedule_auto_delete(client, phdlust.chat.id, phdlust.id, delay=DELETE_AFTER))
+                    await asyncio.sleep(0.2)      
+                    #asyncio.sleep(0.2)
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+                    phdlust = await msg.copy(chat_id=message.from_user.id, caption=caption, reply_markup=reply_markup , protect_content=PROTECT_CONTENT)
+                    phdlusts.append(phdlust)    
+
+            if AUTO_DELETE == True:
+                delete_notification = await message.reply(NOTIFICATION)
+                asyncio.create_task(delete_notification_after_delay(client, delete_notification.chat.id, delete_notification.id, delay=NOTIFICATION_TIME))
+                
+        else:
+            reply_markup = InlineKeyboardMarkup(
+                [[InlineKeyboardButton("𝗔𝗯𝗼𝘂𝘁 𝗠𝗲", callback_data="about"),
+                  InlineKeyboardButton("𝗖𝗹𝗼𝘀𝗲", callback_data="close")]]
+            )
+            await message.reply_text(
+                text=START_MSG.format(
+                    first=message.from_user.first_name,
+                    last=message.from_user.last_name,
+                    username=None if not message.from_user.username else '@' + message.from_user.username,
+                    mention=message.from_user.mention,
+                    id=message.from_user.id
+                ),
+                reply_markup=reply_markup,
+                disable_web_page_preview=True,
+                quote=True
+            )
     else:
         msg = await message.reply(REPLY_ERROR)
         await asyncio.sleep(8)
