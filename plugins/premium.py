fba3a6b9888b8f31f9e14ea67acd26a4c14195df
sukhwinder04 <singhsukhwinder79157@gmail.com>
2025-02-23 20:37:32 +0530

    Update premium.py

--- plugins/premium.py
+++ plugins/premium.py
@@ -1,49 +1,31 @@
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.types import Message
 from bot import Bot
 from config import ADMINS
 from database.database import add_premium_user, remove_premium_user, get_premium_users, is_premium
 
 @Bot.on_message(filters.private & filters.command("prem") & filters.user(ADMINS))
 async def add_premium_command(bot, message: Message):
-    if len(message.command) < 2:
-        return await message.reply("Usage: /add_prem user_id")
+    if len(message.command) < 3:
+        return await message.reply("Usage: /prem user_id time (1min, 1day, 1month)")
     
     user_id = int(message.command[1])
+    duration = message.command[2]
     
-    buttons = [[
-        InlineKeyboardButton("1 Min", callback_data=f"prem_1min_{user_id}"),
-        InlineKeyboardButton("1 Day", callback_data=f"prem_1day_{user_id}"),
-        InlineKeyboardButton("1 Month", callback_data=f"prem_1month_{user_id}")
-    ]]
+    valid_durations = {"1min": "1min", "1day": "1d", "1month": "1m"}
     
-    await message.reply(
-        "Select the premium duration:",
-        reply_markup=InlineKeyboardMarkup(buttons)
-    )
-
-@Bot.on_callback_query(filters.regex(r"prem_(.*)_(\d+)"))
-async def premium_callback(bot, query):
-    duration, user_id = query.data.split("_")[1:]
+    if duration not in valid_durations:
+        return await message.reply("Invalid duration! Use: 1min, 1day, or 1month.")
     
-    if duration == "1min":
-        time = "1min"
-    elif duration == "1day":
-        time = "1d"
-    elif duration == "1month":
-        time = "1m"
-    else:
-        return await query.answer("Invalid selection!", show_alert=True)
-    
-    success = await add_premium_user(int(user_id), time)
+    success = await add_premium_user(user_id, valid_durations[duration])
     if success:
-        await query.message.edit("User successfully added to premium!")
+        await message.reply("User successfully added to premium!")
         try:
-            await bot.send_message(int(user_id), "🎉 You have been added to Premium! Enjoy your benefits!")
+            await bot.send_message(user_id, "🎉 You have been added to Premium! Enjoy your benefits!")
         except:
             pass
     else:
-        await query.answer("Failed to add user to premium!", show_alert=True)
+        await message.reply("Failed to add user to premium!")
 
 @Bot.on_message(filters.private & filters.command("remove_prem") & filters.user(ADMINS))
 async def remove_premium_command(bot, message: Message):
